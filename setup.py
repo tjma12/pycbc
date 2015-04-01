6bf2b0826d25fd37cff929c94289cf71e469453f
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-04-01 13:32:19 -0400

    Added pycbc_make_sngl_workflow into setup.py.

--- setup.py
+++ setup.py
@@ -310,6 +310,7 @@ setup (
                'bin/gstlal/pycbc_pickle_horizon_distances',
                'bin/pycbc_make_coinc_workflow',
                'bin/pycbc_make_daily_workflow',
+               'bin/pycbc_make_sngl_workflow',
                'bin/pycbc_make_html_page',
                'bin/pycbc_ligolw_find_playground',
                'bin/hdfcoinc/pycbc_make_hdf_coinc_workflow',
